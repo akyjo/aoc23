9f10b4c9c66ce119b4990d0a92545ece9e6d0063
akyjo <adamkyjovsky@gmail.com>
2023-12-11 17:06:21 +0100

    day9

---
+++ day9/9p2.py
@@ -0,0 +1,33 @@
+f = open("9.txt", "r")
+msrmnts = [[int(x) for x in line.strip().split()] for line in f.readlines()]
+
+def diffs(series):
+    diff = []
+    diff.append(series)
+
+    def f(s):
+        res = []
+        for i, x in enumerate(s[1:]):
+            res.append(x - s[i])
+        return res
+
+    diff.append(f(series))
+
+    while any(x != 0 for x in diff[-1]):
+        diff.append(f(diff[-1]))
+
+    return diff[:-1]
+
+p = []
+for s in msrmnts:
+    p.append(diffs(s))
+
+
+R = 0
+for l in p:
+    r = 0
+    for x in l[::-1]:
+        r = x[0] - r
+
+    R += r
+print(R)
